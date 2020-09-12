d01868e2d616191a10191217564e446b4110619b
Sagor <mbrsagor@gmail.com>
2020-09-12 10:02:52 +0600

    first commit

---
+++ src/components/calculator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+    state = {}
+    
+    render() { 
+        return (
+            < h1 > I 'm Calculator components</h1>
+        );
+    }
+}
+ 
+export default Calculator;
