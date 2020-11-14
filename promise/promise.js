8bcd05d4fbb23c84d058a7049b66d45faad99360
Sagor <mbrsagor@gmail.com>
2020-11-14 12:24:09 +0600

    Promise arrow function

--- promise/promise.js
+++ promise/promise.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const example1 = () => {
-    var promise = new Promise(function (resolve, reject) {
+    var promise = new Promise((resolve, reject) =>{
         const full_name = "Md.Bozlur Rosid Sagor";
         const nick_name = "mbr-sagor";
         if (full_name === nick_name) {
@@ -23,7 +23,7 @@ example1();
 
 const example2 = () => {
     let is_active = true;
-    new Promise(function (reslove, reject) {
+    new Promise((reslove, reject) =>{
         if (is_active === true) {
             reslove();
         } else {
@@ -35,5 +35,46 @@ const example2 = () => {
         console.log('He/she is an inactive');
     })
 }
-
 example2();
+
+
+const activity = () => {
+    var promise = new Promise((resolve, reject) => {
+        const first_name = 'mbr';
+        const last_name = 'sagor';
+
+        if (first_name === last_name) {
+            resolve();
+        } else {
+            reject();
+        }
+
+    });
+
+    promise.then(function () {
+        console.log("The are same");
+    }).catch(function () {
+        console.log("The are not same")
+    })
+};
+activity();
+
+
+const something = () => {
+    new Promise((resolve, reject) => {
+        is_honest = true;
+        if (is_honest === true) {
+            resolve();
+        } else {
+            reject();
+        }
+    }).then(function () {
+        console.log('He/She is a good guy.');
+    }).catch(function () {
+        console.log('He/She is a bad guy.');
+    })
+}
+something();
+
+
+console.log('I love Programming...');
