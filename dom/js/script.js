7e7f7eaded85c7f2458c0a1991f6e01a938d6b74
Sagor <mbrsagor@gmail.com>
2020-11-16 00:41:19 +0600

    Basic foundation simple webpage using Dom

---
+++ dom/js/script.js
@@ -0,0 +1,17 @@
+// Header
+let header = document.getElementById("header");
+header.style.background = "#f2f2f2";
+header.style.margin = "0";
+
+// Warp
+let warp = document.getElementById("warp");
+warp.style.overflow = "hidden";
+warp.style.width = "800px";
+warp.style.margin = "0 auto";
+
+// Logo
+let logo = document.getElementById("logo");
+// console.log(logo);
+logo.style.background = "#f00";
+logo.style.width = "100px";
+logo.style.float = "left";
