ffbfd0a3ede29c41959ee6441c3ce67f20b87439
Sagor <mbrsagor@gmail.com>
2020-11-15 18:28:34 +0600

    Array map using es6

--- apps.js
+++ apps.js
@@ -10,4 +10,30 @@ for (const [key, val] of Object.entries(info)) {
     console.log(`${key} : ${val}`);
 }
 
-// Splete 
+let new_info = { ...info, name: 'Bozlur Rosid Sagor' };
+console.log(new_info);
+
+// Arrow function
+const say_hello = name => {
+    console.log(`Hello, ${name}\n`);
+}
+say_hello('Sagor');
+
+const informaiton = () => {
+    for (const [x, y] of Object.entries(new_info)) {
+        console.log(`${x} : ${y}`);
+    }
+}
+
+informaiton();
+console.log('\n');
+
+// Array 
+let list = [{ tech: ['laptop', 'mobile', 'desktop', 'TV'] }, 'programming', { languages: ['python', 'javascript'] }, 20, 5]
+console.log(list);
+
+let new_list = [...list, 'Sagor', 'ohi', 'meg']
+// console.log(new_list);
+let _new_item = []
+const item_list = list.map(_item => _new_item.push(_item));
+console.log(item_list);
