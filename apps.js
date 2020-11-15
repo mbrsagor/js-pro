9fc6f2edfd51a3e5888f1896513996e068ec4aeb
Sagor <mbrsagor@gmail.com>
2020-11-15 17:22:38 +0600

    ES6 object iterate

---
+++ apps.js
@@ -0,0 +1,13 @@
+// Object
+let info = {
+    
+    username: 'mbrsagor',
+    age: 26,
+    address: 'Dhaka, Bangladesh'
+}
+// console.log(info);
+for (const [key, val] of Object.entries(info)) {
+    console.log(`${key} : ${val}`);
+}
+
+// Splete 
