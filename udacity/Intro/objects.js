59fee75ab60df76fa2fcd3b3ece8e6f6d29d6d6f
Sagor <mbrsagor@gmail.com>
2020-11-18 02:12:10 +0600

    Add object

---
+++ udacity/Intro/objects.js
@@ -0,0 +1,10 @@
+let information = {
+    name: "sagor",
+    age: 26,
+    phn_num: 17737474709,
+    adddress: "Dhaka-Bangaldesh",
+    is_active: true,
+    languages: ['python', 'javascript', 'Swift', 'Dart']
+};
+
+information.name = "Md.Bozlur Rosid Sagor" // Update value
