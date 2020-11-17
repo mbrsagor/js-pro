05c3f99f5bd6a184f84be6b8982c6af4fc4368d3
Sagor <mbrsagor@gmail.com>
2020-11-18 00:33:39 +0600

    Varialbe to array convert

---
+++ udacity/array.js
@@ -0,0 +1,11 @@
+// Varialbe to array convert
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+// your code goes here
+let crew = new Array(captain, second, pilot, companion, mercenary, mechanic);
+console.log(crew);
