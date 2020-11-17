2887fd103ddbbdb6f1bb55957dd476e22fa0c441
Sagor <mbrsagor@gmail.com>
2020-11-17 22:13:55 +0600

    Start from basic

---
+++ udacity/basic.js
@@ -0,0 +1,7 @@
+// var my_string = "ABC";
+// var ASCII_value = my_string.charCodeAt(0);
+
+var my_string = "Udacity";
+for (var i = 0; i < my_string.length; i++){
+  console.log(my_string.charCodeAt(i));
+}
