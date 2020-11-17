a63975bd737db4508376dce53daa11cba22731fd
Sagor <mbrsagor@gmail.com>
2020-11-17 22:25:17 +0600

    Loop and switch statement

---
+++ udacity/loop.js
@@ -0,0 +1,24 @@
+var x = 10;
+while (x <= 25) {
+  console.log("Printing out x = " + x);
+  x = x + 2;
+}
+
+/**
+ var x = 30;
+while (x >= 0) {
+  if (x === 30) {
+    console.log("30 days")
+  } else if (x === 26) {
+    console.log('Independent day');
+  } else {
+    console.log('Hello world');
+  }
+  x = x + 1;
+}
+ */
+
+
+for (var i = 0; i <= 6; i = i + 2) {
+  console.log(i);
+}
