c4a84755b50907fd4e9b2497d28fa4a0d3ee445d
Sagor <mbrsagor@gmail.com>
2020-11-18 23:57:27 +0600

    Prototypal Inheritance: Subclasses

--- udacity/oop/object/prototype.js
+++ udacity/oop/object/prototype.js
@@ -10,3 +10,22 @@ Dog.prototype.brek = function () {
 
 const dog = new Dog(5, 5.5, 'Suntan');
 dog.brek();
+
+
+// Prototypal Inheritance: Subclasses
+function Animal(name) {
+    this.name = name;
+}
+
+Animal.prototype.walk = function () {
+    console.log(`${this.name} walks`);
+}
+
+function Cat(name) {
+    Animal.call(this.name);
+    this.lives = 9;
+}
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
