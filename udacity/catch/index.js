d92a6b2a866b0e437ede8fe907f721ebd48453c0
Sagor <mbrsagor@gmail.com>
2020-11-24 15:08:16 +0600

    Try catch finally simple example

--- udacity/catch/index.js
+++ udacity/catch/index.js
@@ -38,3 +38,30 @@ new Promise((resolve, reject) => reject(false)).catch((e) => {
     console.log(`Finally ${a}`);
     return 300
 }).then((response) => console.log(response));
+
+// Example 4
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+fetch("http://httpstat.us/500")
+  .then(handleErrors)
+  .then((response) => console.log("ok"))
+  .catch((error) => console.log(error));
+
+// Example 5
+const url = "https://jsonplaceholder.typicode.com/posts/";
+
+async function APIwithcatch() {
+  try {
+    var response = await fetch(url);
+    if (!response.ok) throw await response.json();
+    console.log(response.json());
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+APIwithcatch();
